82fa67320dec98badc91bdce38a73fb57b1d4b62
Mark Blanco <mark.p.blanco@gmail.com>
2020-04-19 10:53:36 -0400

    Remove unused params from TNN test script.

--- test_TNN.py
+++ test_TNN.py
@@ -2,6 +2,7 @@
 # Based off of conv_mnist example in bindnet
 # Uses our temporal neuron node implementation with k-WTA.
 
+import math
 import torch
 import argparse
 import matplotlib.pyplot as plt
@@ -34,16 +35,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--seed", type=int, default=0)
 parser.add_argument("--n_epochs", type=int, default=1)
 parser.add_argument("--n_test", type=int, default=10000)
-parser.add_argument("--kernel_size", type=int, default=16)
 parser.add_argument("--examples", type=int, default=500)
-parser.add_argument("--stride", type=int, default=4)
-parser.add_argument("--n_filters", type=int, default=25)
-parser.add_argument("--padding", type=int, default=0)
 parser.add_argument("--time", type=int, default=50)
 parser.add_argument("--dt", type=int, default=1.0)
 parser.add_argument("--intensity", type=float, default=128.0)
-parser.add_argument("--progress_interval", type=int, default=10)
-parser.add_argument("--update_interval", type=int, default=250)
 parser.add_argument("--train", dest="train", action="store_true")
 parser.add_argument("--test", dest="train", action="store_false")
 parser.add_argument("--plot", dest="plot", action="store_true")
@@ -57,16 +52,10 @@ args = parser.parse_args()
 seed = args.seed
 n_epochs = args.n_epochs
 n_test = args.n_test
-kernel_size = args.kernel_size
-stride = args.stride
-n_filters = args.n_filters
 examples = args.examples
-padding = args.padding
 time = args.time
 dt = args.dt
 intensity = args.intensity
-progress_interval = args.progress_interval
-update_interval = args.update_interval
 train = args.train
 plot = args.plot
 gpu = args.gpu
@@ -177,7 +166,7 @@ for (i, dataPoint) in pbar:
 			ims=spike_ims,
 		)
 		weights_im = plot_weights(
-			get_square_weights(C1.w, 10, 28), 
+			get_square_weights(C1.w, int(math.ceil(math.sqrt(tnn_layer_sz))), 28), 
 			im=weights_im, wmin=0, wmax=max_weight
 		)
 
