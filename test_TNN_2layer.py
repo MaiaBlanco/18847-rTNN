0db0f7663dd4cb1ee86faa89ba1a313e90589a85
pranavdheer <pranavdheer1234@gmail.com>
2020-05-09 19:29:06 -0400

    updated script

--- test_TNN_2layer.py
+++ test_TNN_2layer.py
@@ -1,17 +1,16 @@
-# Test for TNN column implementation
-# Based off of conv_mnist example in bindnet
-# Uses our temporal neuron node implementation with k-WTA.
 
+import torch.nn as nn
 import math
 import torch
 import argparse
 import matplotlib.pyplot as plt
 from torchvision import transforms
+from torch.autograd import Variable
 from time import time as t
 from tqdm import tqdm
+
 from bindsnet.datasets import MNIST
 from bindsnet.encoding import RankOrderEncoder
-from bindsnet.encoding import SingleEncoder
 from bindsnet.network import Network
 from bindsnet.network.monitors import Monitor
 from bindsnet.network.nodes import Input
@@ -20,33 +19,33 @@ from bindsnet.utils import get_square_weights
 from bindsnet.analysis.plotting import (
 	plot_input,
 	plot_spikes,
-    plot_voltages,
-    plot_weights,
+	plot_voltages,
+	plot_weights,
 	plot_conv2d_weights,
 	plot_voltages,
 )
-from TNN import *
-from SpykeTorch import snn
-from SpykeTorch import functional as sf
-from SpykeTorch import visualization as vis
-from SpykeTorch import utils
 
-# Command line parsing
+from TNN import *
+
+print()
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--seed", type=int, default=0)
-parser.add_argument("--n_epochs", type=int, default=1)
-parser.add_argument("--n_test", type=int, default=10000)
-parser.add_argument("--examples", type=int, default=500)
+parser.add_argument("--n_epochs", type=int, default=5000)
+parser.add_argument("--n_test", type=int, default=1000)
+parser.add_argument("--examples", type=int, default=1000)
 parser.add_argument("--time", type=int, default=50)
 parser.add_argument("--dt", type=int, default=1.0)
 parser.add_argument("--intensity", type=float, default=128.0)
 parser.add_argument("--train", dest="train", action="store_true")
 parser.add_argument("--test", dest="train", action="store_false")
 parser.add_argument("--plot", dest="plot", action="store_true")
-parser.add_argument("--gpu", dest="gpu", action="store_true")
 parser.add_argument("--device_id", type=int, default=0)
-parser.set_defaults(plot=False, gpu=False, train=True)
+parser.add_argument("--rf_size", type=int, default=28)
+
+
 args = parser.parse_args()
+rf_size = args.rf_size
 seed = args.seed
 n_epochs = args.n_epochs
 n_test = args.n_test
@@ -56,113 +55,99 @@ dt = args.dt
 intensity = args.intensity
 train = args.train
 plot = args.plot
-gpu = args.gpu
-device_id =  0
-gpu = False
-if gpu and torch.cuda.is_available():
-    torch.cuda.set_device(device_id)
-    # torch.set_default_tensor_type('torch.cuda.FloatTensor')
-else:
-    torch.manual_seed(seed)
+gpu = True
 
-# Parameters for TNN
-input_size = 28*28
-tnn_layer_sz = 25
-tnn_layer_2_sz = 10
-num_timesteps = 16
-tnn_thresh = 256
-tnn_thresh_2 = num_timesteps
-max_weight = num_timesteps/2
-l2_max_weight = max_weight*8
-num_winners = 1
+input_size = rf_size
+tnn_layer_sz = 50
+num_timesteps = 8
+# tnn_thresh = 80
+max_weight = num_timesteps
+# num_winners = 40 #tnn_layer_sz
+
 time = num_timesteps
 
-# TNN Network Build
+torch.manual_seed(seed)
+
+# build network:
 network = Network(dt=1)
 input_layer = Input(n=input_size)
-plot=True
-tnn_layer_1a = TemporalNeurons(
-	n=tnn_layer_sz,
-	timesteps=num_timesteps,
-	threshold=tnn_thresh,
-	num_winners=num_winners
-	)
-tnn_layer_1b = TemporalNeurons(
-	n=tnn_layer_sz,
-	timesteps=num_timesteps,
-	threshold=tnn_thresh,
-	num_winners=num_winners
+tnn_layer_1 = TemporalNeurons( \
+	n=tnn_layer_sz, \
+	timesteps=num_timesteps, \
+	threshold=30, \
+	num_winners=4\
 	)
 
-tnn_layer_2 = TemporalNeurons(
-	n=tnn_layer_2_sz,
-	timesteps=num_timesteps,
-	threshold=tnn_thresh_2,
-	num_winners=1
+tnn_layer_2 = TemporalNeurons( \
+	n=tnn_layer_sz, \
+	timesteps=num_timesteps, \
+	threshold=30, \
+	num_winners=1\
 	)
 
-T1a = Connection(
+buffer_layer_1 = TemporalBufferNeurons(
+	n = tnn_layer_sz,
+	timesteps = num_timesteps,
+	)
+
+C1 = Connection(
 	source=input_layer,
-	target=tnn_layer_1a,
-	w = 0.5 * max_weight * torch.rand(input_layer.n, tnn_layer_1a.n),
+	target=tnn_layer_1,
+	w = 0.01 * max_weight * torch.rand(input_layer.n, tnn_layer_1.n),
 	update_rule=TNN_STDP,
-	ucapture = 	20/128,
-	uminus =	15/128,
-	usearch = 	4/128,
+	ucapture = 	10/128,
+	uminus =	10/128,
+	usearch = 	1/128,
 	ubackoff = 	96/128,
 	umin = 		4/128,
 	timesteps = num_timesteps,
 	maxweight = max_weight
 	)
-T1b = Connection(
-	source=input_layer,
-	target=tnn_layer_1b,
-	w = 0.5 * max_weight * torch.rand(input_layer.n, tnn_layer_1b.n),
+C2 = Connection(
+	source=tnn_layer_1,
+	target=tnn_layer_2,
+	w = 0.01 * max_weight * torch.rand(tnn_layer_1.n, tnn_layer_2.n),
 	update_rule=TNN_STDP,
-	ucapture = 	20/128,
-	uminus =	15/128,
-	usearch = 	4/128,
+	ucapture = 	10/128,
+	uminus =	10/128,
+	usearch = 	1/128,
 	ubackoff = 	96/128,
 	umin = 		4/128,
 	timesteps = num_timesteps,
 	maxweight = max_weight
 	)
 
-T2a = Connection(
-	source=tnn_layer_1a,
-	target=tnn_layer_2,
-	w = 0.5 * max_weight * torch.rand(tnn_layer_1a.n, tnn_layer_2.n),
-	update_rule=TNN_STDP,
-	ucapture = 	10/128,
-	uminus =	10/128,
-	usearch = 	10/128,
-	ubackoff = 	96/128,
-	umin = 		4/128,
-	timesteps = num_timesteps,
-	maxweight = l2_max_weight
+w = torch.diag(torch.ones(tnn_layer_1.n))
+
+TNN_to_buf = Connection(
+	source=tnn_layer_2,
+	target=buffer_layer_1,
+	w = w,
+	update_rule=None
 	)
-T2b = Connection(
-	source=tnn_layer_1b,
-	target=tnn_layer_2,
-	w = 0.5 * max_weight * torch.rand(tnn_layer_1b.n, tnn_layer_2.n),
-	update_rule=TNN_STDP,
-	ucapture = 	10/128,
-	uminus =	10/128,
-	usearch = 	10/128,
-	ubackoff = 	96/128,
-	umin = 		4/128,
+
+buf_to_TNN = Connection(
+	source=buffer_layer_1,
+	target=tnn_layer_1,
+	w = max_weight * torch.rand(tnn_layer_1.n, tnn_layer_1.n),
+	update_rule=None, #TNN_STDP,
 	timesteps = num_timesteps,
-	maxweight = l2_max_weight
+	maxweight = max_weight
 	)
+
+
+
 
 network.add_layer(input_layer, name="I")
-network.add_layer(tnn_layer_1a, name="TNN_1a")
-network.add_layer(tnn_layer_1b, name="TNN_1b")
+network.add_layer(tnn_layer_1, name="TNN_1")
 network.add_layer(tnn_layer_2, name="TNN_2")
-network.add_connection(T1a, source="I", target="TNN_1a")
-network.add_connection(T1b, source="I", target="TNN_1b")
-network.add_connection(T2a, source="TNN_1a", target="TNN_2")
-network.add_connection(T2b, source="TNN_1b", target="TNN_2")
+network.add_layer(buffer_layer_1, name="BUF")
+
+# network.add_connection(C2, source="TNN_1", target="TNN_1")
+network.add_connection(C1, source="I", target="TNN_1")
+network.add_connection(C2, source="TNN_1", target="TNN_2")
+network.add_connection(TNN_to_buf, source="TNN_2", target="BUF")
+network.add_connection(buf_to_TNN, source="BUF", target="TNN_1")
 
 
 spikes = {}
@@ -176,133 +161,111 @@ dataset = MNIST(
 	root=os.path.join("..", "..", "data", "MNIST"),
 	download=True,
 	transform=transforms.Compose(
-		[transforms.ToTensor(), transforms.Lambda(lambda x: x * intensity)]
+		[transforms.CenterCrop(rf_size), transforms.ToTensor(), transforms.Lambda(lambda x: x * intensity)]
 	),
 )
 
-# Added by Kyle for On-Off Encoding
-#dataset = MNIST(
-#	root=os.path.join("..", "..", "data", "MNIST"),
-#	download=True,
-#	transform=preproc,
-#)
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+# train_dataset, test_dataset = dataset.train_data,dataset.test_data
+dataloader = torch.utils.data.DataLoader(
+	train_dataset, batch_size=1, shuffle=False, num_workers=0, pin_memory=False
+)
 
 # Create a dataloader to iterate and batch data
 dataloader = torch.utils.data.DataLoader(
-	dataset, batch_size=1, shuffle=False, num_workers=0, pin_memory=gpu
+	dataset, batch_size=1, shuffle=False, num_workers=0, pin_memory=False
 )
 
-
-inpt_axes = None
-inpt_ims = None
-spike_axes = None
-spike_ims = None
-weights_im = None
-weights_im2 = None
-
+training_pairs = []
 n_iters = examples
 pbar = tqdm(enumerate(dataloader))
 for (i, dataPoint) in pbar:
 	if i > n_iters:
 		break
-	datum = dataPoint["encoded_image"].view(time, 1, 1, 28, 28)#.to(device_id)
+	datum = dataPoint["encoded_image"].view(time, 1, rf_size, rf_size)
 	label = dataPoint["label"]
-	pbar.set_description_str("Pre-train progress: (%d / %d)" % (i, n_iters))
+	pbar.set_description_str("Generate Readout Training Pairs progress: (%d / %d)" % (i, n_iters))
 
-	network.run(inputs={"I": datum}, time=time)
+	for row in range(rf_size):
+		datum_r = datum[:,:,row,:]
+		network.run(inputs={"I": datum_r}, time=time)
 
+	tnn1_spikes = spikes["TNN_1"].get("s").view(time, -1).sum(0)
+	training_pairs.append([tnn1_spikes.squeeze(), label])
 	network.reset_state_variables()
-plot=True
-# training_pairs = []
-pbar = tqdm(enumerate(dataloader))
-n_iters= 1000
-for (i, dataPoint) in pbar:
 
-	if i > n_iters:
-		break
-	datum = dataPoint["encoded_image"].view(time, 1, 1, 28, 28)#.to(device_id)
-	label = dataPoint["label"]
-	pbar.set_description_str("Train progress: (%d / %d)" % (i, n_iters))
+# print(training_pairs)
+# input()
 
-	network.run(inputs={"I": datum}, time=time)
-	# training_pairs.append([spikes["TNN_2"].get("s").int().squeeze(), label])
-	# spikes[layer_name].get("s").view(time, -1).sum(0) 
-    #output_history[sum_over_time]
+# Define logistic regression model using PyTorch.
+class LogisticRegression(nn.Module):
+	def __init__(self, input_dim, output_dim):
+		super(LogisticRegression, self).__init__()
+		self.linear = nn.Linear(input_dim, output_dim)
 
-	if plot and i%10==0:
+	def forward(self, x):
+		out = fn.softmax(self.linear(x))
+		return out
 
-		inpt_axes, inpt_ims = plot_input(
-			dataPoint["image"].view(28, 28),
-			datum.view(time, 784).sum(0).view(28, 28),
-			label=label,
-			axes=inpt_axes,
-			ims=inpt_ims,
-		)
-		spike_ims, spike_axes = plot_spikes(
-			{layer: spikes[layer].get("s").view(time, -1) for layer in spikes},
-			axes=spike_axes,
-			ims=spike_ims,
-		)
-		if (i == 0):
-			for axis in spike_axes:
-				axis.set_xticks(range(time))
-				axis.set_xticklabels(range(time))
+# Create and train logistic regression model on reservoir outputs.
 
-			for l,a in zip(network.layers, spike_axes):
-				a.set_yticks(range(network.layers[l].n))
+# network.train(mode=False)
 
-		weights_im = plot_weights(
-			get_square_weights(T1a.w, int(math.ceil(math.sqrt(tnn_layer_sz))), 28),
-			im=weights_im, wmin=0, wmax=max_weight
-		)
-		weights_im2 = plot_weights(
-			T2a.w,
-			im=weights_im2, wmin=0, wmax=l2_max_weight
-		)
+model = LogisticRegression(tnn_layer_sz,10)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, momentum=0.9)
+logistic_regr_loader = torch.utils.data.DataLoader(dataset=training_pairs, batch_size=32, shuffle=True)
+# Training the Model
+print("\n Training the read out")
+pbar = tqdm(enumerate(range(n_epochs)))
+for epoch, _ in pbar:
+	avg_loss = 0
+	for i, (s, l) in enumerate(logistic_regr_loader):
+		optimizer.zero_grad()
+		outputs = model(s)
+		loss = criterion(outputs, l.squeeze().long())
+		avg_loss += loss.data
+		loss.backward()
+		optimizer.step()
 
+	pbar.set_description_str(
+		"Epoch: %d/%d, Loss: %.4f"
+		% (epoch + 1, n_epochs, avg_loss / len(training_pairs))
+	)
 
-		plt.pause(1e-1)
-	network.reset_state_variables()
-print("\n")
-print("Press enter to continue to evaluation")
-print("\n")
-input()
-
-# TEST LOOP
-table = torch.zeros((tnn_layer_2_sz, 10))
-pred = torch.zeros(tnn_layer_2_sz)
-totals = torch.zeros(tnn_layer_2_sz)
-count = 0
-
-n_iters = examples
+dataloader = torch.utils.data.DataLoader(
+	test_dataset, batch_size=1, shuffle=False, num_workers=0, pin_memory=False
+)
+n_iters = n_test
+test_pairs = []
 pbar = tqdm(enumerate(dataloader))
 for (i, dataPoint) in pbar:
 	if i > n_iters:
 		break
-	datum = dataPoint["encoded_image"].view(time, 1, 1, 28, 28)#.to(device_id)
+	datum = dataPoint["encoded_image"].view(time, 1, rf_size, rf_size)
 	label = dataPoint["label"]
-	pbar.set_description_str("Test progress: (%d / %d)" % (i, n_iters))
+	pbar.set_description_str("Testing progress: (%d / %d)" % (i, n_iters))
 
-	network.run(inputs={"I": datum}, time=time)
-	#training_pairs.append([spikes["TNN_1"].get("s").int().squeeze(), label])
+	for row in range(rf_size):
+		datum_r = datum[:,:,row,:]
+		network.run(inputs={"I": datum_r}, time=time)
 
-	count += 1
-	out = torch.sum(spikes["TNN_2"].get("s").int().squeeze(), dim=0)
-
-	temp = torch.nonzero(out)
-
-	if temp.size(0) != 0:
-		table[temp[0][0], label] += 1
-
+	tnn1_spikes = spikes["TNN_1"].get("s").view(time, -1).sum(0)
+	test_pairs.append([tnn1_spikes.squeeze(), label])
 	network.reset_state_variables()
 
-print("\n\n Confusion Matrix:")
-print(table)
+# Test the Model
+correct, total = 0, 0
+for s, label in test_pairs:
+	outputs = model(s)
+	_, predicted = torch.max(outputs.data.unsqueeze(0), 1)
+	total += 1
+	correct += int(predicted == label.long())
 
-maxval = torch.max(table, 1)[0]
-totals = torch.sum(table, 1)
-pred = torch.sum(maxval)
-covg_cnt = torch.sum(totals)
-
-print("Purity: ", pred/covg_cnt)
-print("Coverage: ", covg_cnt/count)
+print(
+	"\n Accuracy of the model on %d test images: %.2f %%"
+	% (n_iters, 100 * correct / total)
+)
